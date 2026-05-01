46d8459058ff85b8805f4167fa7945243b68b44b
SaouiYassir <yassirsaoui06@gmail.com>
2026-05-01 23:47:08 +0100

    JQuery Added in Header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,24 +1,46 @@
-import './Header.css'
+import { useEffect, useRef } from 'react';
+import './Header.css';
+import $ from 'jquery';
 
 function Header() {
-  
+  const navRef = useRef(null);
+
+  useEffect(() => {
+    const listItems = $(navRef.current).find('li');
+
+    listItems.hover(
+      function() {
+        $(this).css({
+          'color': '#007bff',
+          'transition': '0.3s',
+          'cursor': 'pointer'
+        });
+      },
+      function() {
+        $(this).css('color', 'black');
+      }
+    );
+
+    return () => {
+      listItems.off();
+    };
+  }, []);
+
   return (
-    <>
-      <header>
-        <div className='right-part'>YASSIR SAOUI</div>
-        <div className='list-elements'>
-          <ul>
-            <li>Home</li>
-            <li>Projects</li>
-            <li>Contact</li>
-          </ul>
-        </div>
-        <div className='left-part'>
-          <a href="mailto:yassirsaoui06@gmail.com">Hire me</a>
-        </div>
-      </header>
-    </>
-  )
+    <header>
+      <div className='right-part'>YASSIR SAOUI</div>
+      <div className='list-elements'>
+        <ul ref={navRef}>
+          <li>Home</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      <div className='left-part'>
+        <a href="mailto:yassirsaoui06@gmail.com">Hire me</a>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;

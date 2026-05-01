83c6443678be684658b2f78912e78466b04f0c0a
SaouiYassir <yassirsaoui06@gmail.com>
2026-05-01 20:42:40 +0100

    Version 2 of About Section | Changing Structure

--- src/About.jsx
+++ src/About.jsx
@@ -3,16 +3,9 @@ import './About.css'
 function About() {
     return (
         <div className='all'>
-            <section className='right'>
-                {/* Picture */}
-            </section>
-
-
-            <section className='left'>                
-                <h1>Heeey, I'm Yassir</h1>
-                <h3>Computer Science Student</h3>
-                <p>I’m a motivated student with a strong interest in technology, creativity, and building meaningful projects. I enjoy exploring different fields from programming and game development to design and digital content creation while continuously learning and improving my skills. I like turning ideas into real, practical work, whether it’s developing applications, experimenting with visuals, or working on creative projects. Curious and driven, I’m always looking for new challenges and opportunities to grow both technically and creatively.</p>
-            </section>
+            <h1>Heeey, I'm Yassir</h1>
+            <h3>Computer Science Student</h3>
+            <p>I’m a motivated student with a strong interest in technology, creativity, and building meaningful projects. I enjoy exploring different fields from programming and game development to design and digital content creation while continuously learning and improving my skills. I like turning ideas into real, practical work, whether it’s developing applications, experimenting with visuals, or working on creative projects. Curious and driven, I’m always looking for new challenges and opportunities to grow both technically and creatively.</p>
         </div>
     );
 }

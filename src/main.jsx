f0bc7fab15a66a67fc47e8d7e998b4a39c6169e8
SaouiYassir <yassirsaoui06@gmail.com>
2026-05-01 20:21:34 +0100

    Version 1 of About Section | Left Part

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './Header.jsx'
+import About from './About.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
+    <About />
   </StrictMode>,
 )
